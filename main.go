ca3b76ecd4e95f479f6d2fb82e03634f0bc00ae8
Vaibhav Kubre <v@kubre.in>
2023-08-24 01:02:36 +0530

    Added routs

--- main.go
+++ main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+type LinkForm struct {
+	Link string `json:"link"`
+}
 
 func main() {
-	fmt.Println("Hello World")
+
+	app := fiber.New()
+
+	app.Get("/:code?", func(c *fiber.Ctx) error {
+		code := c.Params("code")
+
+		if code == "" {
+			return c.SendString("Send Post request with { 'url': '' } as request to receive { 'link': '/shorten' } link")
+		}
+
+		return c.JSON(fiber.Map{
+			"success": true,
+			"link":    code,
+		})
+	})
+
+	app.Post("/", func(c *fiber.Ctx) error {
+		linkForm := new(LinkForm)
+
+		if err := c.BodyParser(linkForm); err != nil {
+			return err
+		}
+
+		link := linkForm.Link
+
+		if len(link) == 0 {
+			return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+				"success": false,
+				"error":   "Please add link",
+			})
+		}
+
+		return c.JSON(fiber.Map{
+			"success": true,
+			"link":    link,
+		})
+
+	})
+
+	app.Listen(":80")
 }
